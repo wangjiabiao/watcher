c5b66ca74642db632398e23dd2faaaa7ec72d24c
wangjiabiao <wjb_4595@163.com>
2017-05-23 17:31:44 +0800

    Update unity.py

--- watcher/unity.py
+++ watcher/unity.py
@@ -8,7 +8,8 @@ from subprocess import check_output
 if str(sys.platform).startswith('darwin'):
     EC2ID = '00000000'
 else:
-    EC2ID = check_output(["curl", "-s", 'http://169.254.169.254/latest/meta-data/instance-id']).decode('utf8')
+    #EC2ID = check_output(["curl", "-s", 'http://169.254.169.254/latest/meta-data/instance-id']).decode('utf8')
+    EC2ID = check_output("curl -s http://169.254.169.254/latest/meta-data/instance-id",shell=True).decode('utf8')
 
 if __name__ == '__main__':
     pass
